f25306741ed4d1cb12fb91c4d5ef70fff0927b50
jaclynjessup <jaclynjmoore@gmail.com>
2017-02-08 13:50:16 -0500

    what is added in react

---
+++ react/src/constants/data.js
@@ -0,0 +1,9 @@
+const data = [
+  { id: 1, question: 'Barre', answer: 'The ballet barre is where most of class will be held. You will use the barre for resistance and for general balance. Though you should but be leaning heavily on the barre, it does come in handy when you are on your highest heels.' },
+  { id: 2, question: 'Tube', answer: 'A resitance tube can be a single or double tube (like the one picutred above.)' },
+  { id: 3, question: 'Socks', answer: 'Sitcky Socks' },
+  { id: 4, question: 'Weights', answer: 'You will use weights in the warm up.' },
+  { id: 5, question: 'Ball', answer: 'Ball' }
+];
+
+export default data;
