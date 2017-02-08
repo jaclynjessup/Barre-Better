68a15d00aa6f39fab5b4770f6894953c74b80b87
jaclynjessup <jaclynjmoore@gmail.com>
2017-02-08 14:20:41 -0500

    react & image

--- react/src/constants/data.js
+++ react/src/constants/data.js
@@ -1,9 +1,9 @@
 const data = [
   { id: 1, question: 'Barre', answer: 'The ballet barre is where most of class will be held. You will use the barre for resistance and for general balance. Though you should but be leaning heavily on the barre, it does come in handy when you are on your highest heels.' },
-  { id: 2, question: 'Tube', answer: 'A resitance tube can be a single or double tube (like the one picutred above.)' },
-  { id: 3, question: 'Socks', answer: 'Sitcky Socks' },
-  { id: 4, question: 'Weights', answer: 'You will use weights in the warm up.' },
-  { id: 5, question: 'Ball', answer: 'Ball' }
+  { id: 2, question: 'Tube', answer: 'A resitance tube can be a single or double tube (like the one pitcured). A tube will used from warm up to the cool down stretch.' },
+  { id: 3, question: 'Socks', answer: 'Sitcky Socks will be your new best friend at barre class. These grippy socks will help you keep your form and maintain your footing when in your highest heels.' },
+  { id: 4, question: 'Weights', answer: 'You will use weights in the warm up to help scult your arms. If this is your first class you might want to even try it without weights.' },
+  { id: 5, question: 'Ball', answer: 'It might seem small but the ball is going to give you a challenge. The ball can be used for resistance or to keep your form when in abs.' }
 ];
 
 export default data;
